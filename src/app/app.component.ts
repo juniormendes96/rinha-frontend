2773509ef1edb691bff41cebf030e184e864fe66
Junior <vilmarmjrr@gmail.com>
2023-10-15 15:42:49 -0300

    add file input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,21 +8,22 @@ import { JsonViewerComponent } from './json-viewer.component';
   imports: [CommonModule, JsonViewerComponent],
   template: `
     <main class="h-full w-full p-5">
-      <div *ngIf="!selectedFile; else viewer" class="flex flex-col items-center justify-center h-full">
+      <div *ngIf="!isValidJson; else viewer" class="flex flex-col items-center justify-center h-full">
         <h1 class="text-5xl font-bold mb-4 text-center">JSON Tree Viewer</h1>
         <p class="text-2xl mb-7 text-center">Simple JSON Viewer that runs completely on-client. No data exchange</p>
+        <input type="file" class="hidden" #fileInput (change)="onFileSelected($event)" />
         <button
           type="button"
           class="bg-gray-200 border-solid border border-black py-2 px-4 rounded font-medium hover:brightness-95"
-          (click)="selectedFile = 'any file'"
+          (click)="fileInput.click()"
         >
           Load JSON
         </button>
-        <p *ngIf="isJsonInvalid" class="text-red-600 mt-4">Invalid file. Please load a valid JSON file.</p>
+        <p *ngIf="hasTypeError" class="text-red-600 mt-4">Invalid file. Please load a valid JSON file.</p>
       </div>
       <ng-template #viewer>
         <div class="m-auto w-full max-w-5xl">
-          <h1 class="text-3xl font-bold">alltypes.json</h1>
+          <h1 class="text-3xl font-bold">{{ selectedFile?.name }}</h1>
           <rf-json-viewer></rf-json-viewer>
         </div>
       </ng-template>
@@ -32,6 +33,22 @@ import { JsonViewerComponent } from './json-viewer.component';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  selectedFile?: string;
-  isJsonInvalid = false;
+  selectedFile: File | null = null;
+
+  get isValidJson(): boolean {
+    return !!this.selectedFile && !this.hasTypeError;
+  }
+
+  get hasTypeError(): boolean {
+    return !!this.selectedFile && this.selectedFile.type !== 'application/json';
+  }
+
+  onFileSelected(event: Event): void {
+    this.selectedFile = this.extractFile(event);
+  }
+
+  private extractFile(event: Event): File | null {
+    const files = (event.target as HTMLInputElement)?.files;
+    return files ? files[0] : null;
+  }
 }
